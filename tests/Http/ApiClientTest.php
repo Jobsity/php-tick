2f02c48a96918a70f24018eda1e6abd6069a3f7a
Yudeikis Orta <yudeikis.orta@jobsity.com>
2015-11-13 09:12:14 -0500

    Update ApiClientTest to cover exceptions

--- tests/Http/ApiClientTest.php
+++ tests/Http/ApiClientTest.php
@@ -1,214 +1,405 @@
 <?php
-namespace Jobsity\PhpTick\Test;
+namespace Jobsity\PhpTick\Test\Http;
 
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Jobsity\PhpTick\Http\ApiClient;
+use Jobsity\PhpTick\Http\Exception\ApiException;
 use mef\Log\StandardLogger;
 use PHPUnit_Framework_TestCase;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
 
 /**
  * Class ApiClientTest
  *
- * @package Jobsity\PhpTick\Test
+ * @package Jobsity\PhpTick\Test\Http
+ *
+ * @coversDefaultClass Jobsity\PhpTick\Http\ApiClient
  */
-class ApiClientTest extends PHPUnit_Framework_TestCase{
-
-    /**
-     * @var string User's subscription id
-     */
-    protected $subscriptionId;
-
-    /**
-     * @var string User's access token
-     */
-    protected $accessToken;
-
-    /**
-     * @var string User's company
-     */
-    protected $company;
-
-    /**
-     * @var string User's email
-     */
-    protected $email;
-
-    /**
-     * ApiClientTest Setup
-     */
+class ApiClientTest extends PHPUnit_Framework_TestCase
+{
     protected function setup()
     {
         $this->subscriptionId = '456789';
         $this->accessToken = '2387654321234578';
         $this->company = 'Company';
         $this->email = 'some@company.com';
+        $this->logger = new StandardLogger();
     }
 
     /**
-     * Test Get Request
+     * @covers ::getInstance
      *
-     * @covers ApiClient::get
-     * @uses ApiClient::__construct
-     *
-     * @param string    $endpoint      Final endpoint
-     * @param array     $queryParams   Parameters for querying
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
      */
-    public function getRequestSuccessResponse($endpoint, array $queryParams)
+    public function testGetInstance()
     {
-        $responseParams = json_encode([['id'=>'8989985'], ['id' => '2323232']]);
-        $mock = new MockHandler([
-            new Response(200, [], $responseParams),
-            new Response(401),
-            new Response(500)
-        ]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
-
-        $logger = new StandardLogger();
-
-        $apiClient = new ApiClient($client, $logger, $this->subscriptionId, $this->accessToken, $this->company,
-            $this->email);
-
-        $response = $apiClient->get($endpoint, $queryParams);
-        $this->assertTrue(is_array($response));
-        $this->assertJsonStringEqualsJsonString($responseParams, json_encode($response));
-
-        $response = $apiClient->get($endpoint, $queryParams);
-        $this->assertEquals(401, $response);
-
-        $response = $apiClient->get($endpoint, $queryParams);
-        $this->assertEquals(500, $response);
-    }
-
-    /**
-     * Test Post Request
-     *
-     * @param string    $endpoint   Final endpoint
-     * @param array     $data       Data to insert
-     *
-     * @covers ApiClient::post
-     * @uses ApiClient::__construct
-     */
-    public function postRequestSuccessResponse($endpoint, array $data)
-    {
-        $responseParams = json_encode(['id' => '48917908', 'date' => "2015-11-11", 'hours' => '6',
-            'task_id' => '1235', 'notes' => 'some']);
-        $mock = new MockHandler([
-            new Response(200, [], $responseParams),
-            new Response(401),
-            new Response(500)
-        ]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
-
-        $logger = new StandardLogger();
-
-        $apiClient = new ApiClient($client, $logger, $this->subscriptionId, $this->accessToken, $this->company,
-            $this->email);
-
-        $response = $apiClient->post($endpoint, $data);
-        $this->assertJsonStringEqualsJsonString($responseParams, json_encode($response));
-
-        $response = $apiClient->post($endpoint, $data);
-        $this->assertEquals(401, $response);
-
-        $response = $apiClient->post($endpoint, $data);
-        $this->assertEquals(500, $response);
-    }
-
-    /**
-     * Test Put Request
-     *
-     * @param string    $endpoint   Final endpoint
-     * @param array     $data       Data to update
-     *
-     * @covers ApiClient::put
-     * @uses ApiClient::__construct
-     */
-    public function putRequestSuccessResponse($endpoint, array $data)
-    {
-        $responseParams = json_encode(['hours' => '2', 'notes' => 'some test v2']);
-        $mock = new MockHandler([
-            new Response(200, [], $responseParams),
-            new Response(401),
-            new Response(500)
-        ]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
-
-        $logger = new StandardLogger();
-
-        $apiClient = new ApiClient($client, $logger, $this->subscriptionId, $this->accessToken, $this->company,
-            $this->email);
-
-        $response = $apiClient->put($endpoint, $data);
-        $this->assertEquals(200, $response);
-
-        $response = $apiClient->put($endpoint, $data);
-        $this->assertEquals(401, $response);
-
-        $response = $apiClient->put($endpoint, $data);
-        $this->assertEquals(500, $response);
-    }
-
-    /**
-     * Test Delete Request
-     *
-     * @param string    $endpoint   Final endpoint
-     *
-     * @covers ApiClient::delete
-     * @uses ApiClient::__construct
-     */
-    public function deleteRequestSuccessResponse($endpoint)
-    {
-        $responseParams = json_encode(['hours' => '2', 'notes' => 'some test v2']);
-        $mock = new MockHandler([
-            new Response(204, [], $responseParams),
-            new Response(401),
-            new Response(500)
-        ]);
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
-
-        $logger = new StandardLogger();
-
-        $apiClient = new ApiClient($client, $logger, $this->subscriptionId, $this->accessToken, $this->company,
-            $this->email);
-
-        $response = $apiClient->delete($endpoint);
-        $this->assertEquals(204, $response);
-
-        $response = $apiClient->delete($endpoint);
-        $this->assertEquals(401, $response);
-
-        $response = $apiClient->delete($endpoint);
-        $this->assertEquals(500, $response);
-    }
-
-    /**
-     * Funcion to call all the tests with parameters
-     */
-    public function testGeneral(){
-        $this->getRequestSuccessResponse('entries', ['updated_at' => '2015-11-11']);
-
-        $postData = [
-            'date' => '2015-11-11',
-            'hours' => '5',
-            'task_id' => '1235',
-            'notes' => 'some'
-        ];
-        $this->postRequestSuccessResponse('entries', $postData);
-
-        $putData = array(
-            'hours' => '2',
-            'notes' => 'some test v2'
+        $apiClient = ApiClient::getInstance(
+            $this->subscriptionId,
+            $this->accessToken,
+            $this->company,
+            $this->email
         );
-        $this->putRequestSuccessResponse('entries/5678976', $putData);
 
-        $this->deleteRequestSuccessResponse('entries/5678976');
+        $this->assertInstanceOf(ApiClient::class, $apiClient);
     }
 
+    /**
+     * @covers ::__construct
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     */
+    public function testConstructor()
+    {
+        $apiClient = $this->getClientApi();
+
+        $this->assertInstanceOf(ApiClient::class, $apiClient);
+
+        return $apiClient;
+    }
+
+    /**
+     * @covers ::get
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     *
+     */
+    public function testGetSuccess()
+    {
+        $firstId = '8989985';
+
+        $data = json_encode([
+            ['id' => $firstId],
+            ['id' => '2323232']
+        ]);
+
+        $apiClient = $this->getClientApi([new Response(200, [], $data)]);
+
+        $response = $apiClient->get('endpoint', []);
+
+        $this->assertInternalType('array', $response);
+        $this->assertCount(2, $response);
+        $this->assertArrayHasKey('id', $response[0]);
+        $this->assertEquals($firstId, $response[0]['id']);
+    }
+
+    /**
+     * @covers ::get
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testGetUnauthorizedFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(401) ]);
+
+        try {
+            $response = $apiClient->get('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(401, $e->getErrorCode());
+            $this->assertEquals('unauthorized', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::get
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testGetServerFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(500) ]);
+
+        try {
+            $response = $apiClient->get('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(500, $e->getErrorCode());
+            $this->assertEquals('internal server error', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::get
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testGetRequestFailure()
+    {
+        $apiClient = $this->getClientApi([
+            new RequestException("Something went wrong",
+            new Request('GET', 'endpoint'))]);
+
+        try {
+            $response = $apiClient->get('endpoint', []);
+        } catch (Exception $e) {
+            $this->assertEquals('something went wrong', strtolower($e->getMessage()));
+        }
+    }
+
+    /**
+     * @covers ::post
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     *
+     */
+    public function testPostSuccess()
+    {
+        $taskId = '34344443';
+        $data = json_encode([
+            ['date' => '2015-11-10'],
+            ['hours' => '5'],
+            ['task_id' => $taskId],
+            ['notes' => 'some test v2']
+        ]);
+
+        $apiClient = $this->getClientApi([new Response(200, [], $data)]);
+
+        $response = $apiClient->post('endpoint', []);
+
+        $this->assertInternalType('array', $response);
+        $this->assertCount(4, $response);
+        $this->assertArrayHasKey('task_id', $response[2]);
+        $this->assertEquals($taskId, $response[2]['task_id']);
+    }
+
+    /**
+     * @covers ::post
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testPostUnauthorizedFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(401) ]);
+
+        try {
+            $response = $apiClient->post('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(401, $e->getErrorCode());
+            $this->assertEquals('unauthorized', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::post
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testPostServerFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(500) ]);
+
+        try {
+            $response = $apiClient->post('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(500, $e->getErrorCode());
+            $this->assertEquals('internal server error', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::post
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testPostRequestFailure()
+    {
+        $apiClient = $this->getClientApi([
+            new RequestException("Something went wrong",
+            new Request('POST', 'endpoint'))]);
+
+        try {
+            $response = $apiClient->post('endpoint', []);
+        } catch (Exception $e) {
+            $this->assertEquals('something went wrong', strtolower($e->getMessage()));
+        }
+    }
+
+    /**
+     * @covers ::put
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     *
+     */
+    public function testPutSuccess()
+    {
+        $data = json_encode([
+            ['hours' => '2'],
+            ['notes' => 'some test v2']
+        ]);
+
+        $apiClient = $this->getClientApi([new Response(204, [], $data)]);
+
+        $response = $apiClient->put('endpoint', []);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals('no content', strtolower($response->getReasonPhrase()));
+    }
+
+    /**
+     * @covers ::put
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testPutUnauthorizedFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(401) ]);
+
+        try {
+            $response = $apiClient->put('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(401, $e->getErrorCode());
+            $this->assertEquals('unauthorized', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::put
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testPutServerFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(500) ]);
+
+        try {
+            $response = $apiClient->put('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(500, $e->getErrorCode());
+            $this->assertEquals('internal server error', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::put
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testPutRequestFailure()
+    {
+        $apiClient = $this->getClientApi([
+            new RequestException("Something went wrong",
+            new Request('PUT', 'endpoint'))]);
+
+        try {
+            $response = $apiClient->put('endpoint', []);
+        } catch (Exception $e) {
+            $this->assertEquals('something went wrong', strtolower($e->getMessage()));
+    }
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     *
+     */
+    public function testDeleteSuccess()
+    {
+        $apiClient = $this->getClientApi([ new Response(204) ]);
+
+        $response = $apiClient->delete('endpoint', []);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals('no content', strtolower($response->getReasonPhrase()));
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testDeleteUnauthorizedFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(401) ]);
+
+        try {
+            $response = $apiClient->delete('endpoint');
+        } catch (ApiException $e) {
+            $this->assertEquals(401, $e->getErrorCode());
+            $this->assertEquals('unauthorized', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testDeleteServerFailure()
+    {
+        $apiClient = $this->getClientApi([ new Response(500) ]);
+
+        try {
+            $response = $apiClient->delete('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(500, $e->getErrorCode());
+            $this->assertEquals('internal server error', strtolower($e->getErrorMessage()));
+        }
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\Exception\ApiException
+     *
+     */
+    public function testDeleteRequestFailure()
+    {
+        $apiClient = $this->getClientApi([
+            new RequestException("Something went wrong",
+            new Request('DELETE', 'endpoint'))]);
+
+        try {
+            $response = $apiClient->delete('endpoint', []);
+        } catch (ApiException $e) {
+            $this->assertEquals(500, $e->getErrorCode());
+            $this->assertEquals('internal server error', strtolower($e->getErrorMessage()));
+        } catch (Exception $e) {
+            $this->assertEquals('something went wrong', strtolower($e->getMessage()));
+        }
+    }
+
+    private function getClientApi(array $responses = [])
+    {
+        $mock = new MockHandler($responses);
+        $handler = HandlerStack::create($mock);
+        $guzzleClient = new Client(['handler' => $handler]);
+
+        $apiClient = new ApiClient(
+            $guzzleClient,
+            $this->logger,
+            $this->subscriptionId,
+            $this->accessToken,
+            $this->company,
+            $this->email
+        );
+
+        return $apiClient;
+    }
 }
