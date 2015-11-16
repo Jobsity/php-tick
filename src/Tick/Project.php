ff15a2a5dd285cfe2047209fde99fc1bd4049039
Yudeikis Orta <yudeikis.orta@jobsity.com>
2015-11-16 09:15:09 -0500

    Project class and tests

--- src/Tick/Project.php
+++ src/Tick/Project.php
@@ -1,2 +1,163 @@
 <?php
 namespace Jobsity\PhpTick\Tick;
+
+use InvalidArgumentException;
+use Jobsity\PhpTick\Http\ClientInterface;
+use Jobsity\PhpTick\Http\ApiClient;
+
+/**
+ * Class Project
+ *
+ * @package Jobsity\PhpTick\Tick
+ */
+class Project
+{
+    /**
+     * @var ApiClient Guzzle Api Client Handler
+     */
+    private $client;
+
+    /**
+     * Constructs Project.
+     *
+     * @param ClientInterface   $client     Guzzle Api Client Handler.
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Get opened projects list.
+     *
+     * @return mixed
+     */
+    public function getList()
+    {
+        return $this->client->get('projects');
+    }
+
+    /**
+     * Get closed projects list.
+     *
+     * @return mixed
+     */
+    public function getListClosed()
+    {
+        return $this->client->get('projects/closed');
+    }
+
+    /**
+     * Get detail of one project.
+     *
+     * @param string    $projectId     Task id.
+     *
+     * @return mixed
+     */
+    public function get($projectId)
+    {
+        return $this->client->get('projects/' . $projectId);
+    }
+
+    /**
+     * Get entries of an specific project.
+     *
+     * @param string    $projectId  Project Id.
+     * @return mixed
+     */
+    public function getEntries($projectId)
+    {
+        return $this->client->get('projects/' . $projectId . '/entries');
+    }
+
+    /**
+     * Create project.
+     *
+     * @param string    $name               Project name.
+     * @param string    $clientId           Client id of the project.
+     * @param string    $ownerId            Owner id of the project.
+     * @param string    $budget             Project's budget.
+     * @param boolean   $notifications      Notifications enabled.
+     * @param boolean   $billable           If it's billable.
+     * @param boolean   $recurring          If it's recurring.
+     *
+     * @return mixed
+     */
+    public function create($name, $clientId, $ownerId, $budget = null, $notifications = true, $billable = true,
+                           $recurring = false)
+    {
+        $params = [
+            'name'  => $name,
+            'client_id' => $clientId,
+            'owner_id' => $ownerId,
+            'budget' => $budget,
+            'notifications' => $notifications,
+            'billable' => $billable,
+            'recurring' => $recurring
+        ];
+
+        return $this->client->post('projects/', $params);
+    }
+
+    /**
+     * Create project.
+     *
+     * @param string    $projectId          Project id.
+     * @param string    $name               Project name.
+     * @param string    $clientId           Client id of the project.
+     * @param string    $ownerId            Owner id of the project.
+     * @param string    $budget             Project's budget.
+     * @param boolean   $notifications      Notifications enabled.
+     * @param boolean   $billable           If it's billable.
+     * @param boolean   $recurring          If it's recurring.
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return mixed
+     */
+    public function update($projectId, $name = null, $clientId = null, $ownerId = null, $budget = null,
+                           $notifications = null, $billable = null, $recurring = null)
+    {
+        if(!$name && !$clientId && !$ownerId && !$budget && !$notifications && !$billable && !$recurring) {
+            throw new InvalidArgumentException('You must specify at least one attribute for update.');
+        }
+
+        $params = [];
+
+        if($name) {
+            $params['name'] = $name;
+        }
+        if($clientId) {
+            $params['client_id'] = $clientId;
+        }
+        if($ownerId) {
+            $params['owner_id'] = $ownerId;
+        }
+        if($budget) {
+            $params['budget'] = $budget;
+        }
+        if($notifications) {
+            $params['notifications'] = $notifications;
+        }
+        if($billable) {
+            $params['billable'] = $billable;
+        }
+        if($recurring) {
+            $params['recurring'] = $recurring;
+        }
+
+        return $this->client->put('tasks/' . $projectId, $params);
+    }
+
+    /**
+     * Delete project.
+     *
+     * @param string    $projectId    Project id to be deleted.
+     *
+     * @return mixed
+     */
+    public function delete($projectId)
+    {
+        return $this->client->delete('projects/' . $projectId);
+    }
+}
---
+++ tests/Tick/ProjectTest.php
@@ -0,0 +1,254 @@
+<?php
+namespace Jobsity\PhpTick\Test\Tick;
+
+use PHPUnit_Framework_TestCase;
+use InvalidArgumentException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use mef\Log\StandardLogger;
+use Jobsity\PhpTick\Http\ApiClient;
+use Jobsity\PhpTick\Tick\Project;
+
+/**
+ * Class ProjectTest
+ *
+ * @package Jobsity\PhpTick\Test\Tick
+ *
+ * @coversDefaultClass Jobsity\PhpTick\Tick\Project
+ */
+class ProjectTest extends PHPUnit_Framework_TestCase
+{
+    protected function setup()
+    {
+        $this->subscriptionId = '456789';
+        $this->accessToken = '2387654321234578';
+        $this->company = 'Company';
+        $this->email = 'some@company.com';
+        $this->logger = new StandardLogger();
+    }
+
+    /**
+     * @covers ::__construct
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     */
+    public function testConstructor()
+    {
+        $project = $this->projectHandler();
+
+        $this->assertInstanceOf(Project::class, $project);
+
+        return $project;
+    }
+
+    /**
+     * @covers ::getList
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::get
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testGetListSuccess()
+    {
+        $firstId = '8989985';
+
+        $data = json_encode([
+            ['id' => $firstId, 'name' => 'project test'],
+            ['id' => '2323232', 'name' => 'other project']
+        ]);
+
+        $projectList = $this->projectHandler([ new Response(200, [], $data )])->getList();
+
+        $this->assertInternalType('array', $projectList);
+        $this->assertCount(2, $projectList);
+        $this->assertArrayHasKey('name', $projectList[0]);
+        $this->assertEquals($firstId, $projectList[0]['id']);
+    }
+
+    /**
+     * @covers ::getListClosed
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::get
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testGetListClosedSuccess()
+    {
+        $firstId = '8989985';
+
+        $data = json_encode([
+            ['id' => $firstId, 'name' => 'project test'],
+            ['id' => '2323232', 'name' => 'other project']
+        ]);
+
+        $projectList = $this->projectHandler([ new Response(200, [], $data )])->getListClosed();
+
+        $this->assertInternalType('array', $projectList);
+        $this->assertCount(2, $projectList);
+        $this->assertArrayHasKey('name', $projectList[0]);
+        $this->assertEquals($firstId, $projectList[0]['id']);
+    }
+
+    /**
+     * @covers ::get
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::get
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testGet()
+    {
+        $id = '3456843';
+        $data = json_encode([
+            ['id' => $id, 'name' => 'project test']
+        ]);
+
+        $project = $this->projectHandler([ new Response(200, [], $data) ])->get($id);
+
+        $this->assertInternalType('array', $project);
+        $this->assertCount(1, $project);
+        $this->assertArrayHasKey('name', $project[0]);
+        $this->assertEquals($id, $project[0]['id']);
+    }
+
+    /**
+     * @covers ::getEntries
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::get
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testGetEntries()
+    {
+        $id = '3456843';
+        $data = json_encode([
+            ['id' => $id, 'name' => 'entry'],
+            ['id' => '3598566', 'name' => 'some entry']
+        ]);
+
+        $project = $this->projectHandler([ new Response(200, [], $data) ])->getEntries($id);
+
+        $this->assertInternalType('array', $project);
+        $this->assertCount(2, $project);
+        $this->assertArrayHasKey('name', $project[0]);
+        $this->assertEquals($id, $project[0]['id']);
+    }
+
+    /**
+     * @covers ::create
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::post
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testCreate()
+    {
+        $name = 'some more';
+        $clientId = '797899';
+        $ownerId = '2563';
+
+        $data = json_encode([
+            ['name' => $name, 'client_id' => $clientId, 'owner_id' => $ownerId]
+        ]);
+
+        $project = $this->projectHandler([ new Response(200, [], $data) ])->create($name, $clientId, $ownerId);
+
+        $this->assertInternalType('array', $project);
+        $this->assertCount(1, $project);
+        $this->assertEquals($name, $project[0]['name']);
+    }
+
+    /**
+     * @covers ::update
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::put
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testUpdate()
+    {
+        $projectId = '3434214';
+        $name = 'some more';
+        $clientId = '434343';
+        $ownerId = '34343';
+        $budget = '2.5';
+        $notifications = true;
+        $billable = true;
+        $recurring = true;
+
+        $data = json_encode([
+            ['project' => $projectId, 'name' => $name]
+        ]);
+
+        $project = $this->projectHandler([ new Response(200, [], $data) ])->update($projectId, $name, $clientId,
+            $ownerId, $budget, $notifications, $billable, $recurring);
+
+        $this->assertInternalType('array', $project);
+        $this->assertCount(1, $project);
+        $this->assertEquals($projectId, $project[0]['project']);
+    }
+
+    /**
+     * @covers ::update
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::put
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testUpdateFailure()
+    {
+        try {
+            $project = $this->projectHandler()->update('797899');
+        } catch (InvalidArgumentException $e) {
+            $this->assertEquals('you must specify at least one attribute for update.', strtolower($e->getMessage()));
+        }
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @uses Jobsity\PhpTick\Http\ApiClient::getInstance
+     * @uses Jobsity\PhpTick\Http\ApiClient::__construct
+     * @uses Jobsity\PhpTick\Http\ApiClient::delete
+     * @uses Jobsity\PhpTick\Tick\Project::__construct
+     */
+    public function testDelete()
+    {
+        $projectId = '3343434';
+
+        $response = $this->projectHandler([ new Response(200) ])->delete($projectId);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    private function projectHandler(array $responses = [])
+    {
+        $mock = new MockHandler($responses);
+        $handler = HandlerStack::create($mock);
+        $guzzleClient = new Client(['handler' => $handler]);
+
+        $apiClient = new ApiClient(
+            $guzzleClient,
+            $this->logger,
+            $this->subscriptionId,
+            $this->accessToken,
+            $this->company,
+            $this->email
+        );
+
+        $project = new Project($apiClient);
+
+        return $project;
+    }
+
+}
